9c7ab88e240d5ad5d74b34090ca5371e209b6ebd
Evaneta <eftimovaevaneta@gmail.com>
2023-02-10 20:59:43 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,43 @@
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
+  const [text, setText] = useState("")
+
+  const update = (e) => {
+    setText(e.target.value)
+  }
+
+  const saveToLS = () => {
+    localStorage.setItem('note', JSON.stringify(text));
+  }
+
+  const reset =()=> {
+    setText("");
+  }
+
+  useEffect(() => {
+    const items = JSON.parse(localStorage.getItem('note'));
+    if (items) {
+      saveToLS()
+      setText(text);
+    }
+  }, []);
+
   return (
     <div className="App">
       <div className="box">
         <div className="field">
           <div className="control">
-            <textarea className="textarea is-large" placeholder="Notes..." />
+            <textarea value={text} onChange={update} className="textarea is-large" placeholder="Notes..." />
           </div>
         </div>
-        <button className="button is-large is-primary is-active">Save</button>
-        <button className="button is-large">Clear</button>
+        <button className="button is-large is-primary is-active" onClick={saveToLS.bind(this)}>Save</button>
+        <button className="button is-large" onClick={reset.bind(this)}>Clear</button>
       </div>
     </div>
   );
 }
 
 export default App;
+
